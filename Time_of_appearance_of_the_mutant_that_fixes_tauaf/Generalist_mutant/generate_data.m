59f924a88c1c03848c342f873b10e053a0e3d7be
LcMrc <69910283+LcMrc@users.noreply.github.com>
2020-08-19 18:16:56 +0200

    Create generate_data.m

---
+++ Time_of_appearance_of_the_mutant_that_fixes_tauaf/Generalist_mutant/generate_data.m
@@ -0,0 +1,18 @@
+% Code created by Loïc Marrec
+
+% Parameters
+
+n = 5;                          % Hill coefficient
+theta = 1e3;                    % Inflection time
+gW = 0.1;                       % Death rate of W microbes
+XW_i = 10;                      % Initial number of W microbes
+fG = .5;                        % Fitness of G microbes
+gG = 0.1;                       % Death rate of G microbes
+XG_i = 0;                       % Initial number of G microbes
+K = 1e3;                        % Carrying capacity
+mu = 1e-5;                      % Mutation probability upon division
+Nit = 1e3;                      % Number of stochastic realizations 
+
+% Simulation
+
+tauaf = Gillespie_fct(Nit, n, theta, gW, XW_i, fG, gG, XG_i, K, mu);
